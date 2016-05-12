2d61c12eb04604b011a72a7edb8342d7300c223e
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2016-05-12 00:21:54 -0500

    python ex
    exercises to practice python lenguaje solving problems from hacker rank

---
+++ python/lambda_map.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/map-and-lambda-expression
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(raw_input())
+
+fibo=lambda a: fibo(a-1)+fibo(a-2) if a>1 else (0 if a==0 else 1)
+cubeFibo=lambda a: pow(fibo(a),3)
+ar=map(cubeFibo,range(0,n))
+print ar
