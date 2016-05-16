af3314db988b88b8c1a9a1c09ca2e138fcfb5202
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2016-05-15 19:36:54 -0500

    interchange two numbers
    excercise to interchange two inputs

---
+++ python/nterchange_two_numbers.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/interchange-two-numbers
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+ar=1,
+ar=(raw_input(),raw_input())
+a,b=ar
+print b
+print a
