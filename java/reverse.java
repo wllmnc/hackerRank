389a6e051fbc88577d7fc53a2b9991e68bb4f658
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2016-05-28 22:26:02 -0500

    reverse java first attemp
    first incomplete attemp

---
+++ java/reverse.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String B=A.substring(A.length()/2);
+        String C=A.substring(0,A.length()/2);
+        ini=0
+        if(B.length()>C.length())
+            ini=1
+        String reverse="";
+        for(ini,ini<(B.length();ini++)
+            Reverse
+        System.out.println(C);
+        
+    }
+}
