c11da848f70be762404e5eb2ccc15889a643e270
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2016-12-09 23:27:50 -0600

    create c-tutorial-basic-data-types
    using cin and couit we receive and print the different types of values.

---
+++ cpp/c-tutorial-basic-data-types.cpp
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int       a;
+    long      b; 
+    long long c;
+    char      d;
+    float     e;
+    double    f;  
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << a << endl << b << endl << c << endl << d << endl << e << endl << f;
+    return 0;
+}
