2ffd286b46eb5f5259d27fd865df059e3e0c2844
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2016-12-30 21:53:22 -0600

    create ruby-tutorial-each
    to practice and learn to use each in ruby

---
+++ ruby/ruby-tutorial-each.rb
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/ruby-tutorial-each
+def scoring(array)
+    # iterate through each of the element in array using *each* and call update_score on it
+    array.each do |user|
+        user.update_score()
+    end
+end
