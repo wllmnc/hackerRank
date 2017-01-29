6761e5de027fa8e00e840f26bdc76b76bcd53df9
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2017-01-28 23:21:27 -0600

    Create handshake.cpp
    solution at the problem to looking for the number of handshakes in a room with N people

---
+++ Mathematics/Fundamentals/handshake.cpp
@@ -0,0 +1,39 @@
+//https://www.hackerrank.com/challenges/handshake
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int T;
+    cin >> T;
+    for(int a0 = 0; a0 < T; a0++){
+        int N;
+        cin >> N;
+        cout << ((N - 1)*N)/2 << endl;
+    }
+    
+    return 0;
+}
+
