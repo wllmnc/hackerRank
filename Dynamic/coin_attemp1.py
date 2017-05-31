864d8f67d6982bdbe2add365674d0686544ebffa
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2017-05-30 22:14:35 -0500

    Create coin_attemp1.py
    first attempt to determine how many ways there are to give a change from a specific coins

---
+++ Dynamic/coin_attemp1.py
@@ -0,0 +1,22 @@
+# Dynamic Programming Python implementation of Coin Change problem
+def count(S, m, n):
+    table = [[0 for x in range(m)] for x in range(n+1)]
+    for i in range(m):
+        table[0][i] = 1
+ 
+    # Fill rest of the table enteries in bottom up manner
+    for i in range(1, n+1):
+        for j in range(m):
+            # Count of solutions including S[j]
+            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+ 
+            # Count of solutions excluding S[j]
+            y = table[i][j-1] if j >= 1 else 0
+ 
+            # total count
+            table[i][j] = x + y
+ 
+    return table[n][m-1]
+    
+arr = [1, 2, 3]
+print(count(arr, len(arr), len(arr)+1))
