eb5b8b8be55eea87da40044a0d779942721fba5f
William Enrique Nunez Chavez <wnunez.mx@gmail.com>
2017-06-15 23:37:33 -0500

    Update transform-to-palindrome.py
    optimization of the first attempt but is still failing by recursion I presume, so I need to improve this scope or figure out if there is any index out of the limits making overflow.

--- contest/w33/transform-to-palindrome.py
+++ contest/w33/transform-to-palindrome.py
@@ -1,46 +1,37 @@
 #!/bin/python
 #https://www.hackerrank.com/contests/w33/challenges/transform-to-palindrome
 import sys
-
-
-
-def getTransform(destination,path):
-    ans=-1
-    last_=path[len(path)-1]
-    if last_ in paths:
-        for candidate in paths[last_]:
-            if candidate not in path:
-                if candidate ==destination:
-                    return candidate
-                else:
-                    path.append(candidate)
-                    return getTransform(destination,path)
+sys.setrecursionlimit(100000000)
+def expandTree(arr_,a):
+    ans=arr_
+    for item in a:        
+        if not item in ans:
+            ans.append(item)        
+            ans_=expandTree(ans,paths[item])
+            for item2 in ans_:
+                if not item2 in ans:
+                    ans.append(item2)            
     return ans
 
-def getMinimunMovs(a,i_,j_):
+def istherePath(origin,destination):
+    return destination in paths[origin]
+
+def getMinimunMovs(i_,j_):
     ans_=0
     i,j=i_,j_
-    noItems=[]
-    while(i!=j and j>i):
+    while(i<j):
         if a[i]!=a[j]:
-            prev1_=getTransform(a[j],[a[i]])
-            prev2_=getTransform(a[i],[a[j]])
-            if prev1_+prev2_==-2:
-                if j-i==1:
-                    ans_=1
-                else:
-                    ans1=getMinimunMovs(a,i+1,j)
-                    ans1= ans1 if ans1>=0 else n+1
-                    ans2=getMinimunMovs(a,i,j-1)
-                    ans2=ans2 if ans2>=0 else n+1
-                    ans_=1+min(ans1,ans2)
-                    i=j-1
+            if not istherePath(a[j],a[i]) and not istherePath(a[i],a[j]):
+                ans_=1+min(getMinimunMovs(i+1,j),getMinimunMovs(i,j-1))
+                break
         i+=1
         j-=1
     return ans_
+
 n,k,m = raw_input().strip().split(' ')
 n,k,m = [int(n),int(k),int(m)]
 paths={}
+
 for a0 in xrange(k):
     x,y = raw_input().strip().split(' ')
     x,y = [int(x),int(y)]
@@ -52,6 +43,9 @@ for a0 in xrange(k):
         paths[y].append(x)    
     else:
         paths[y]=[x]
-#print paths
+        
+for item in paths.keys():
+    paths[item]=expandTree([],paths[item])
+
 a = map(int, raw_input().strip().split(' '))
-print len(a)-getMinimunMovs(a,0,len(a)-1)
+print len(a)-getMinimunMovs(0,len(a)-1)
